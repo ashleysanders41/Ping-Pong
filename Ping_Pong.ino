3b86bb028f94713df17453713022cad2dd910866
ashleysanders41 <ashleymatt4101@gmail.com>
2014-10-22 12:50:51 -0700

    Update Ping_Pong.ino

--- Ping_Pong.ino
+++ Ping_Pong.ino
@@ -1,26 +1,26 @@
-import org.firmata.*;
-import cc.arduino.*;
+import org.firmata.*; //importing standard firmata
+import cc.arduino.*; // importing arduino library
 
-import processing.serial.*;              
-import java.awt.Rectangle;              
+import processing.serial.*;  // importing processing serial port           
+import java.awt.Rectangle;  // importing java sketch            
 
-Serial myPort;    
+Serial myPort;   // my serial port 
 
 
-Rectangle leftPaddle, rightPaddle;        
+Rectangle leftPaddle, rightPaddle; // left and rigth paddles on screen       
 
-int paddleHeight = 85;            
-int paddleWidth = 10;             
-int ballSize = 10;     
-int xDirection = 5;   
-int yDirection = 5;    
-int xPos, yPos;  
-int rectSize = 25;
-int leftScore = 0;     
-int rightScore = 0;     
-int fontSize = 20;          
-int topLeftx = 350;
-int topLefty = 100;
+int paddleHeight = 85; //height of paddle        
+int paddleWidth = 10;  //width of paddle         
+int ballSize = 10;  // size of ball   
+int xDirection = 5;  //inital direction (horizontal) 
+int yDirection = 5;  // initial direction (vertical)  
+int xPos, yPos;  // initial horizonal and vertical positions of paddle
+int rectSize = 25; // size of paddles
+int leftScore = 0; // left side player score 
+int rightScore = 0;  // right side player score   
+int fontSize = 20;  // font size of score        
+int topLeftx = 350; //initial position of score
+int topLefty = 100; // initial position of score
 int middleLeftx = 200;
 int middleLefty = 250;
 int bottomLeftx = 350;
@@ -28,47 +28,48 @@ int bottomLefty = 400;
 
 void setup() {
 
- size(1000, 500);      
+ size(1000, 500); // size of screen     
 
  
- leftPaddle = new Rectangle(50, height/2, paddleWidth, paddleHeight);
- rightPaddle = new Rectangle(width-50, height/2, paddleWidth, paddleHeight);
+ leftPaddle = new Rectangle(50, height/2, paddleWidth, paddleHeight); //initial value of left paddle
+ rightPaddle = new Rectangle(width-50, height/2, paddleWidth, paddleHeight); //initial values of right paddle
 
  noStroke(); 
- fill(600);  
+ fill(600); //color of ball 
 
- resetBall();
+ resetBall(); //return ball to middle once someone scores
 
+// font size and text name
  PFont myFont = createFont(PFont.list()[2], fontSize);
  textFont(myFont);
    
  
  println(Serial.list());
- myPort = new Serial(this, Serial.list()[5], 9600);
+ myPort = new Serial(this, Serial.list()[5], 9600); //my serial port connected with arduino
  
  myPort.bufferUntil('\n');
 }
 
 void draw() {
  
- background(300);
- fill(250,0,0);
- //rect(405,0,10,500);
- fill(250,250,250);
- //rect(1000,25,0,0)
+ background(300); //background color
+ fill(250,0,0); //background fill
+ //rect(405,0,10,500); //deleted line in the middle of screen
+ fill(250,250,250); //background fill
+ //rect(1000,25,0,0) //deleted line in middle of screen
 
  
- rect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
- rect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
- //rect(topLeftx, topLefty, 25, 25);
- //rect(middleLeftx, middleLefty, 25, 25);
- //rect(bottomLeftx, bottomLefty, 25, 25);
+ rect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height); //dimensions of left paddle
+ rect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height); //dimensions of right paddle
+ //rect(topLeftx, topLefty, 25, 25); // deleted rectangle
+ //rect(middleLeftx, middleLefty, 25, 25); //deleted rectangle
+ //rect(bottomLeftx, bottomLefty, 25, 25); //deleted rectangle
  
- animateBall();
+ animateBall(); //animate ball
 
  
- text(leftScore, fontSize, fontSize);
- text(rightScore, width-fontSize, fontSize);
+ text(leftScore, fontSize, fontSize); //left score's score, font, and size
+ text(rightScore, width-fontSize, fontSize); //right score's score, font, and size
 }
 
 
